f9666589fa128d47cfb47e57cb163ea8dd0250f6
Nicholas-James Clavet <nicholas-james.clavet@hec.ca>
2026-05-26 10:47:08 -0400

    Ajustement du paramètre de dernière année

--- srpp/srpp.py
+++ srpp/srpp.py
@@ -93,7 +93,7 @@ class rules:
         self.yrspars = self.yrspars.set_index('year')
         self.cpi = 0.02
         self.wgr = 0.03
-        self.lastyear = 2022
+        self.lastyear = 2026
         self.indexation = np.ones((2100-1966,2100-1966))
         ones_lower = np.tril(self.indexation)
         for y in range(2100-1966):
